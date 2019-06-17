05df369ecb787aaa8bb729f7ac2e11e58dc3b058
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-06-17 16:02:09 +0100

    Add task sorting, filtering & pagination

--- src/routers/Task.js
+++ src/routers/Task.js
@@ -17,9 +17,27 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+    const { completed, limit, skip, sortBy } = req.query
+    if (completed) {
+        match.completed = (completed === 'true')
+    }
+    if(sortBy){
+        const parts = sortBy.split(':')
+        sort[parts[0]] = (parts[1] === 'desc') ? -1 : 1
+    }
     try {
-        const tasks = await Task.find({ owner: req.user._id });
-        res.send(tasks);
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(limit),
+                skip: parseInt(skip),
+                sort
+            }
+        }).execPopulate()
+        res.send(req.user.tasks);
     } catch (e) {
         res.status(500).send(e);
     }
