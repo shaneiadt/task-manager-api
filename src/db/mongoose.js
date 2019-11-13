7c4bcdd10492eaea8000932119260b253c188e22
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-11-13 12:09:44 +0000

    Update mongoose orm package

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,6 @@ const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
 
 mongoose.set('useFindAndModify', false);
 
-mongoose.connect(connectionURL, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+mongoose.connect(connectionURL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err) => {
     if (err) return console.log(err);
 });
